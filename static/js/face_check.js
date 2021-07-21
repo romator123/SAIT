6b39ad918ba8d9ee8694ea133c6793c3d341f57c
Eldar485 <takhtarov2016@yandex.ru>
2021-07-21 14:10:59 +0300

    Терминал

--- static/js/face_check.js
+++ static/js/face_check.js
@@ -1,1 +1,31 @@
-<script src="face-api.js"><script src="face-api.js"></script>
+const video = document.getElementById('video')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector,
+  faceapi.nets.faceLandmark68Net,
+  faceapi.nets.faceRecognitionNet,
+  faceapi.nets.faceExpressionNet
+]).then(startVideo)
+
+function startVideo() {
+  navigator.navigator.mediaDevices.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
